17adca8cb6d4ef0e60d56ae40061331d4f138eca
dustin.sappier <dustin.sappier@learningspurs.com>
2016-01-05 16:18:17 -0400

    added menu - add function - list function

--- src/Lists.cpp
+++ src/Lists.cpp
@@ -10,20 +10,59 @@
 #include "List.h"
 using namespace std;
 
-int main() {
-	Node* mitch = new Node("Mitch");
-	Node* dustin = new Node("Dustin");
-	Node* ray = new Node("Ray");
-	List* list = new List();
-	list->add(mitch);
-	list->add(dustin);
-	list->add(ray);
-	Node* i = list->getFirst();
-	while(i != NULL)
-	{
-		cout << i -> getName() << endl;
-		i = i ->getNext();
-	}
+int main();
 
-	return 0;
+char cont;
+List* list = new List();
+string data;
+
+void deleteItem() {
+
+}
+
+void add() {
+	do{
+		cout << "Enter an item: " << endl;
+		cin >> data;
+		Node* temp = new Node(data);
+		list->add(temp);
+		cout << "Would you like to continue adding items? (y/n): " << endl;
+		cin >> cont;
+	}while(cont == 'y');
+	main();
+}
+
+void listItem() {
+	Node* i = list->getFirst();
+	while (i != NULL) {
+		cout << i->getItem() << endl;
+		i = i->getNext();
+	};
+	main();
+}
+
+int main() {
+	int choice;
+	do {
+		cout << "1. Add item" << endl << "2. Delete item" << endl
+				<< "3. Show list" << endl << "4. Exit" << endl;
+		cin >> choice;
+		switch (choice) {
+		case 1:
+			add();
+			break;
+		case 2:
+			deleteItem();
+			break;
+		case 3:
+			listItem();
+			break;
+		case 4:
+			cout << "Exiting.." << endl;
+			break;
+		default:
+			cout << "Invalid selection.. " << endl;
+			main();
+		}
+	} while (choice < 1 || choice > 4);
 }
